07d2a22a90196fd8dbce18e89a7d10679669908a
hardik <hardik96500c@gmail.com>
2024-03-29 20:38:56 +0530

    update package.json

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,11 @@ app.use("/server/msg", msgRouter);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
   app.use(cookieParser());
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
